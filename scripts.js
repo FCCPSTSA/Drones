c8f19cac0ae0e48f927394f3c9385b0bb271319f
ErikBoesen <erik.boesen@gmail.com>
2015-12-18 19:16:19 -0500

    Slanted thing yay

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
 $(window).scroll(function () {
 	var wScroll = $(window).scrollTop();
-	$('header').css('background-position', '0% ' + -0.65 * wScroll + 'px');
+	//$('header').css('background-position', '0% ' + -0.65 * wScroll + 'px');
 });
