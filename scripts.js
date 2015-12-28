37d4539bfc9a1106a3e72a856c4e8c095e5c4c27
ErikBoesen <erik.boesen@gmail.com>
2015-12-28 09:31:13 -0500

    Added more content and improved style a bit.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,12 @@
 $(window).scroll(function () {
 	var wScroll = $(window).scrollTop();
-	//$('header').css('background-position', '0% ' + -0.65 * wScroll + 'px');
+	$('header').css('background-position', '0% ' + 0.2 * wScroll + 'px');
 });
+
+/*
+$('nav a').on('click', function () {
+	$('html, body').animate({
+		scrollTop: window.innerHeight * (Math.floor(($(window).scrollTop() / window.innerHeight)) + 1)
+	}, 500);
+});
+*/
